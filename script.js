751f0cc6bbc93f312f4c674ae3fb455447ca9366
Martin Väli <martinvali12@gmail.com>
2021-09-19 20:02:16 +0300

    Change cors mode

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 (async function () {
   const startingLoaders = await fetch("https://vanillaloaders.herokuapp.com/", {
-    mode: "cors",
+    mode: "no-cors",
   });
+  console.log(startingLoaders);
   const selectShapeEl = document.querySelector(".select-shape");
   selectShapeEl.addEventListener("change", function () {
     console.log(this);
