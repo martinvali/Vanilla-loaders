f8299f57792dd3bf27925a36ede013d497cafd29
Martin Väli <martinvali12@gmail.com>
2021-12-22 20:38:55 +0200

    Add Flexbox to articles

--- script.js
+++ script.js
@@ -9,6 +9,10 @@
     const article = document.createElement("article");
     article.innerHTML = element.html;
     article.classList.add("spinner-article");
+    article.style.display = "flex";
+    article.style.justifyContent = "center";
+    article.style.alignItems = "center";
+
     dataContainer.append(article);
   });
   const selectShapeEl = document.querySelector(".select-shape");
