b1d699eb3808948b3c4bd1b4c24d75a11fff846e
Martin Väli <martinvali12@gmail.com>
2021-12-28 16:40:51 +0200

    add a loading class to the container before real animations are inserted

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 
   for (let i = 1; i <= 20; i++) {
     const article = document.createElement("article");
-    article.classList.add("spinner-article");
+    article.classList.add("spinner-article", "loading");
     dataContainer.append(article);
   }
   const startingLoaders = await fetch("https://vanillaloaders.herokuapp.com/", {
@@ -12,6 +12,7 @@
   const data = await startingLoaders.json();
   const articles = document.querySelectorAll(".spinner-article");
   data.forEach((element, index) => {
+    articles[index].classList.remove("loading");
     articles[index].innerHTML = element.html;
   });
   const selectShapeEl = document.querySelector(".select-shape");
