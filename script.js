9811a0ee1a6f829d56367069014a44631dbc8f35
Martin Väli <martinvali12@gmail.com>
2021-12-28 16:28:36 +0200

    Modify javascript

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 (async function () {
+  for (let i = 1; i <= 20; i++) {
+    const dataContainer = document.querySelector(".spinners-container");
+    const article = document.createElement("article");
+    article.innerHTML = element.html;
+    article.classList.add("spinner-article");
+    dataContainer.append(article);
+  }
   const startingLoaders = await fetch("https://vanillaloaders.herokuapp.com/", {
     mode: "cors",
   });
   const data = await startingLoaders.json();
-  const dataContainer = document.querySelector(".spinners-container");
-
-  data.forEach((element) => {
-    const article = document.createElement("article");
-    article.innerHTML = element.html;
-    article.classList.add("spinner-article");
-
-    dataContainer.append(article);
+  const articles = document.querySelectorAll(".spinner-article");
+  data.forEach((element, index) => {
+    articles[index].append(element);
   });
   const selectShapeEl = document.querySelector(".select-shape");
   selectShapeEl.addEventListener("change", function () {
